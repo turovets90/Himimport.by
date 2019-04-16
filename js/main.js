1c93884c339514eb40a9975a0b101906e58ed3a4
turovets90 <a.turovets90@gmail.com>
2019-04-16 15:54:18 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -125,114 +125,29 @@ $(document).ready(function(){
     $(".phone").mask("+375 (99) 999-99-99");
 
 
-
-
-
-
-    /*
-
-
-    $('.ms_prev').click(function(){
-        $('.main_slider').slick('slickPrev');
-    });
-
-    $('.ms_next').click(function(){
-        $('.main_slider').slick('slickNext');
-    });
-
-
-
-    if($(".categories_slider .slider_item").length >6){
-        $('.categories_slider').slick({
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            arrows: true,
-            dots:false,
-            infinite:false,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 5,
-                    }
-                },
-                {
-                    breakpoint: 1100,
-                    settings: {
-                        slidesToShow: 4,
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-
-                {
-                    breakpoint: 575,
-                    settings: {
-                        slidesToShow: 2,
-                    }
-                },
-                {
-                    breakpoint: 375,
-                    settings: {
-                        slidesToShow: 1,
-                    }
-                }
-            ]
-        });
-    }
-
-
-    $('.filter_item').each(function(){
-        var filter_head=$(this).find('.filter_head');
-        var filter_body=$(this).find('.filter_body');
-        var filter_item=$(this);
-        $(filter_head).click(function(){
-            $('.filter_item').removeClass('b_0 active');
-            $('.filter_body').slideUp();
-            if($(filter_body).is(':visible')){
-                $(filter_body).slideUp();
-                $(filter_item).removeClass('active');
-                $(filter_item).prev().removeClass('b_0');
-            }else{
-                $(filter_body).slideDown();
-                $(filter_item).addClass('active');
-                $(filter_item).prev().addClass('b_0');
+    $(window).resize(function(){
+        var header = $('header').outerHeight();
+        console.log(header);
+        $(window).scroll(function(){
+            if ($(window).scrollTop() > header) {
+                $('header').addClass('fixed');
+                $('body').css({
+                    'padding-top':header+'px'
+                });
+            } else {
+                $('header').removeClass('fixed');
+                $('body').css({
+                    'padding-top':'0px'
+                });
             }
         });
+
+
+
     });
+    $(window).resize();
 
 
-    var news_grid=$('.news_grid');
-    if(news_grid){
-        $(news_grid).masonry({
-            itemSelector: '.grid-item',
-        });
-    }
-
-
-
-    $('.product_main_slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        //fade: true,
-        asNavFor: '.product_slider_nav'
-    });
-
-    $('.product_slider_nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-        dots:true,
-        asNavFor: '.product_main_slider',
-        focusOnSelect: true
-    });
-
-*/
 
 
 });
