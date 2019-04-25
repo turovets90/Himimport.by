d855537f39e009d5dd299bdffc3d90db1fd7a4bb
turovets90 <a.turovets90@gmail.com>
2019-04-25 20:45:40 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -149,6 +149,21 @@ $(document).ready(function(){
 
 
 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrolltop').fadeIn();
+        } else {
+            $('.scrolltop').fadeOut();
+        }
+    });
+
+    $('.scrolltop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+
+
 
 });
 
